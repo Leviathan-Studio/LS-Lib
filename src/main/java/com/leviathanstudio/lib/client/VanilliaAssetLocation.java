96887b3d051a4254b5d7548b6920533b647c202a
Phenix246 <florian.gomes246@gmail.com>
2016-09-15 21:06:48 +0200

    Add some vanillia resource location

---
+++ src/main/java/com/leviathanstudio/lib/client/VanilliaAssetLocation.java
@@ -0,0 +1,55 @@
+package com.leviathanstudio.lib.client;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public class VanilliaAssetLocation
+{
+    public static final ResourceLocation MINECRAFT_LOGO             = new ResourceLocation("textures/gui/title/minecraft.png");
+
+    public static final ResourceLocation GUI_BARS                   = new ResourceLocation("textures/gui/bars.png");
+    public static final ResourceLocation GUI_BOOK                   = new ResourceLocation("textures/gui/book.png");
+    public static final ResourceLocation GUI_DEMO_BACKGROUND        = new ResourceLocation("textures/gui/demo_background.png");
+    public static final ResourceLocation GUI_ICONS                  = new ResourceLocation("textures/gui/icons.png");
+    public static final ResourceLocation GUI_OPTION_BACKGROUND      = new ResourceLocation("textures/gui/options_background.png");
+    public static final ResourceLocation GUI_RESOURCE_PACK          = new ResourceLocation("textures/gui/resource_packs.png");
+    public static final ResourceLocation GUI_SERVER_SELECTION       = new ResourceLocation("textures/gui/server_selection.png");
+    public static final ResourceLocation GUI_SPECTATOR_WIDGETS      = new ResourceLocation("textures/gui/spectator_widgets.png");
+    public static final ResourceLocation GUI_STREAM_INDICATOR       = new ResourceLocation("textures/gui/stream_indicator.png");
+    public static final ResourceLocation GUI_WIDGETS                = new ResourceLocation("textures/gui/widgets.png");
+    public static final ResourceLocation GUI_WORLD_SELECTION        = new ResourceLocation("textures/gui/world_selection.png");
+
+    public static final ResourceLocation GUI_ACHIEVEMENT_BACKGROUND = new ResourceLocation("textures/gui/achievement/achievement_background.png");
+
+    public static final ResourceLocation GUI_ANVIL                  = new ResourceLocation("textures/gui/container/anvil.png");
+    public static final ResourceLocation GUI_BEACON                 = new ResourceLocation("textures/gui/container/beacon.png");
+    public static final ResourceLocation GUI_BREWING_STAND          = new ResourceLocation("textures/gui/container/brewing_stand.png");
+    public static final ResourceLocation GUI_CRAFTING_TABLE         = new ResourceLocation("textures/gui/container/crafting_table.png");
+    public static final ResourceLocation GUI_DISPENSER              = new ResourceLocation("textures/gui/container/dispenser.png");
+    public static final ResourceLocation GUI_ENCHANTING_TABLE       = new ResourceLocation("textures/gui/container/enchanting_table.png");
+    public static final ResourceLocation GUI_FURNACE                = new ResourceLocation("textures/gui/container/furnace.png");
+    public static final ResourceLocation GUI_CHEST                  = new ResourceLocation("textures/gui/container/generic_54.png");
+    public static final ResourceLocation GUI_HOPPER                 = new ResourceLocation("textures/gui/container/hopper.png");
+    public static final ResourceLocation GUI_HORSE                  = new ResourceLocation("textures/gui/container/horse.png");
+    public static final ResourceLocation GUI_INVENTORY              = new ResourceLocation("textures/gui/container/inventory.png");
+    public static final ResourceLocation GUI_STATS_ICONS            = new ResourceLocation("textures/gui/container/stats_icons.png");
+    public static final ResourceLocation GUI_VILLAGER               = new ResourceLocation("textures/gui/container/villager.png");
+
+    public static final ResourceLocation FOOTPRINT                  = new ResourceLocation("textures/particule/footprint.png");
+    public static final ResourceLocation PARTICLES                  = new ResourceLocation("textures/particule/particles.png");
+
+    public static final ResourceLocation PAINTING                   = new ResourceLocation("textures/painting/paintings_kristoffer_zetterstrand.png");
+
+    public static final ResourceLocation MAP_BACKGROUND             = new ResourceLocation("textures/map/map_background.png");
+    public static final ResourceLocation MAP_ICON                   = new ResourceLocation("textures/map/map_icons.png");
+
+    public static final ResourceLocation MOON                       = new ResourceLocation("textures/environment/moon_phases.png");
+    public static final ResourceLocation RAIN                       = new ResourceLocation("textures/environment/rain.png");
+    public static final ResourceLocation SUN                        = new ResourceLocation("textures/environment/sun.png");
+
+    public static final ResourceLocation ASCII                      = new ResourceLocation("textures/font/ascii.png");
+    public static final ResourceLocation ASCII_SGA                  = new ResourceLocation("textures/font/ascii_sga.png");
+
+}
