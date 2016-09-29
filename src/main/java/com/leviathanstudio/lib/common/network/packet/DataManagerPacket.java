06dc02ec34e361b312616e9d0c2ebc2f33c3880f
Phenix246 <florian.gomes246@gmail.com>
2016-09-29 18:38:17 +0200

    Fix data manager packet

--- src/main/java/com/leviathanstudio/lib/common/network/packet/DataManagerPacket.java
+++ src/main/java/com/leviathanstudio/lib/common/network/packet/DataManagerPacket.java
@@ -1,6 +1,7 @@
 package com.leviathanstudio.lib.common.network.packet;
 
 import com.leviathanstudio.lib.common.data2.DataManagers;
+import com.leviathanstudio.lib.common.network.ByteBufUtil;
 
 import net.minecraft.entity.player.EntityPlayer;
 
@@ -10,11 +11,14 @@ import io.netty.channel.ChannelHandlerContext;
 /**
  * Data Manager Packet use in the LSL Network system. This packet process data
  * contain in the data manager and handle serialization and deserialization.
- * Please add an empty. constructor for the system.
  */
 public class DataManagerPacket extends SyncAbstractPacket
 {
     private String name;
+
+    public DataManagerPacket()
+    {
+    }
 
     public DataManagerPacket(String manager)
     {
@@ -24,12 +28,14 @@ public class DataManagerPacket extends SyncAbstractPacket
     @Override
     public void encodeInto(ChannelHandlerContext ctx, ByteBuf buffer)
     {
+        ByteBufUtil.writeUTF8String(buffer, this.name);
         DataManagers.getEntry(this.name).writeBuffer(buffer);
     }
 
     @Override
     public void decodeInto(ChannelHandlerContext ctx, ByteBuf buffer)
     {
+        this.name = ByteBufUtil.readUTF8String(buffer);
         DataManagers.getEntry(this.name).readBuffer(buffer);
     }
 
